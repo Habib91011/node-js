981760cfd9e50ea2ec007724aa47035c51e46701
Habib91011 <131193899+Habib91011@users.noreply.github.com>
2023-09-30 22:28:46 +0300

    employee.js

--- employee.js
+++ employee.js
@@ -5,6 +5,7 @@ var employeeSchema = new Schema({
     firstName:{ type: String, required: true },
     lastName:{ type: String, required: true },
     email: { type: String, required: true },
+    time: { type: String, required: true },
     password: String
 });
 
